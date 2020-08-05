edb190fb330752b77ce424caea83392cde28f0a0
SqbikaPC <sqbika@gmail.com>
2020-08-05 08:09:29 +0200

    Init

---
+++ WAMTConfig.cs
@@ -0,0 +1,11 @@
+﻿using Exiled.API.Interfaces;
+
+namespace WhoAreMyTeammates
+{
+    public class WAMTConfig : IConfig
+    {
+        public bool IsEnabled { get; set; }
+
+        public bool IncludeNames { get; set; } = false;
+    }
+}
--- WhoAreMyTeammatesPlugin.cs
+++ WhoAreMyTeammatesPlugin.cs
@@ -1,8 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exiled.API.Features;
 
 namespace WhoAreMyTeammates
 {
-    public class Class1
+    public class WhoAreMyTeammatesPlugin : Plugin<WAMTConfig>
     {
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted += OnNewRound;
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= OnNewRound;
+        }
+
+        public override void OnReloaded()
+        {
+            OnEnabled();
+            OnDisabled();
+        }
+
+        private void OnNewRound()
+        {
+            List<Player> scps = Player.List.Where(player => player.Team == Team.SCP).ToList();
+            StringBuilder builder = new StringBuilder();
+            builder.Append("SCP Team:");
+            foreach (Player player in scps)
+            {
+                builder.Append(", ").Append(player.Role);
+                if (Config.IncludeNames)
+                {
+                    builder.Append("(").Append(player.Nickname).Append(")");
+                }
+            }
+            String result = builder.ToString();
+            foreach (Player player in scps)
+            {
+                player.ClearBroadcasts();
+                player.Broadcast(15, result);
+            }
+        }
     }
 }
